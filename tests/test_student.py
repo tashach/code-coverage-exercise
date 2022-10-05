2d108448a2068da609aabdfa167bc574079ca891
Tasha <tashachang2@gmail.com>
2022-10-05 13:43:21 -0700

    Completed code coverage

--- tests/test_student.py
+++ tests/test_student.py
@@ -11,6 +11,17 @@ def test_init():
     assert ada.level == level
     assert ada.courses == courses
 
+def test_init_courses_None():
+    name = "Ada Lovelace"
+    level = "sophomore"
+    courses = None
+
+    ada = Student(name, level, courses)
+
+    assert ada.name == name
+    assert ada.level == level
+    assert ada.courses == [ ]
+
 def test_add_class():
     new_class = 'Intro to Feminism'
     charles = Student("Charles Babbage", "senior", ["mechanical engineering"])
@@ -40,5 +51,10 @@ def test_get_student_with_more_classes():
         "sophomore",
         ["mathematics", "foundations of computing"]
     )
-
+    
+    result = get_student_with_more_classes(charles,ada)
+    result2 = get_student_with_more_classes(ada,charles)
+    
+    assert result == ada
+    assert result2 == ada
     # TODO: write assertions
